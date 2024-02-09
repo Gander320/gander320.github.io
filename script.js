1efc6bbb9aac2212cd27c0991e36a31f14b8f524
Gander320 <94793709+Gander320@users.noreply.github.com>
2024-02-09 17:39:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function() {
     const calendarContainer = document.getElementById('calendar');
     const scheduleForm = document.getElementById('scheduleForm');
